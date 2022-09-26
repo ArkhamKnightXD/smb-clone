0bd003d01370a0c349ceeea272e95499c6440380
karvin <karvin.jimenez@alternard.com>
2022-09-26 13:47:44 -0400

    extra changes

--- core/src/main/java/knight/arkham/helpers/Box2DBody.java
+++ core/src/main/java/knight/arkham/helpers/Box2DBody.java
@@ -4,8 +4,6 @@ import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Shape;
 import com.badlogic.gdx.physics.box2d.World;
 
-import static knight.arkham.helpers.Constants.PIXELS_PER_METER;
-
 public class Box2DBody {
 
 //    Utilizaré un vector para guardar la posición en X y Y, para tener menos variables, y es recomendado esta forma
@@ -19,9 +17,7 @@ public class Box2DBody {
 
     public Box2DBody(Vector2 position, float width, float height, World world) {
 
-// Hago esto aqui para no tener que dividir a la hora de crear mi body. Es posible que generar otro
-// vector2 sea algo innecesario, pero de esta forma automatizo más la creación de mis body2D. Ojo con esto
-        this.position = new Vector2(position.x / PIXELS_PER_METER,position.y / PIXELS_PER_METER);
+        this.position = position;
         this.width = width;
         this.height = height;
         this.world = world;
@@ -29,7 +25,7 @@ public class Box2DBody {
 
     public Box2DBody(Vector2 position, World world, Shape shape) {
 
-        this.position = new Vector2(position.x / PIXELS_PER_METER,position.y / PIXELS_PER_METER);
+        this.position = position;
         this.world = world;
         this.shape = shape;
     }
